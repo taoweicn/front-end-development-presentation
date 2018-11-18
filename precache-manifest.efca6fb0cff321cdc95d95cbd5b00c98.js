04bf6f03761aade2603964ce0ad465aa322bfb28
taoweicn <twchn@live.com>
2018-11-18 20:18:19 +0800

    Updates

---
+++ precache-manifest.efca6fb0cff321cdc95d95cbd5b00c98.js
@@ -0,0 +1,48 @@
+self.__precacheManifest = [
+  {
+    "url": "vendor.4.b907b06c31f3ce498e7d.bundle.js"
+  },
+  {
+    "url": "main.0.b67cf9927621c5afd6df.css"
+  },
+  {
+    "url": "presenter.host.1.2f8f16baa737dc6f12d3.css"
+  },
+  {
+    "url": "presenter.host.1.2f8f16baa737dc6f12d3.bundle.js"
+  },
+  {
+    "url": "presenter.view.2.f10582fbe771e069366c.bundle.js"
+  },
+  {
+    "url": "runtime.84450dc906e5425b85ea.bundle.js"
+  },
+  {
+    "url": "vendor.4.b907b06c31f3ce498e7d.css"
+  },
+  {
+    "url": "vendors~Sidebar.5.9ffbb2dc721f84418480.bundle.js"
+  },
+  {
+    "url": "main.0.b67cf9927621c5afd6df.bundle.js"
+  },
+  {
+    "revision": "26de010f9852c6f2d7adf8c7499c83e5",
+    "url": "index.html"
+  },
+  {
+    "url": "c4804131a06b2a6d0ddde42c278e7802.png"
+  },
+  {
+    "url": "a1c606395f23cb23a43aa4e0600f39b1.gif"
+  },
+  {
+    "url": "6eff166b40e47ddcbfe5e8725f508708.png"
+  },
+  {
+    "url": "6c3503ac264cb2ee221f7f9379250f02.jpg"
+  },
+  {
+    "url": "1fba9e5d60669ec02ffc7d03068afae0.jpg"
+  }
+];
